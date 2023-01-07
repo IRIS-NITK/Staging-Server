3cced091f58cb206e1f4493f7ff1636c4bf8936a
rishi <mrrishi646@gmail.com>
2023-01-08 01:57:41 +0530

    minor changes

--- main/tasks/services.py
+++ main/tasks/services.py
@@ -561,8 +561,8 @@ def deploy_from_git(self, token, url, social, org_name, repo_name, branch_name, 
 
     if org_name == "NITK-IRIS":
         src = f'{PATH_TO_HOME_DIR}/{org_name}/{repo_name}/{branch_name}/{repo_name}/' + "config/initializers"
-        path_to_nitk_setting = os.getenv(PATH_TO_NITK_SETTING)
-        path_to_secret_token = os.getenv(PATH_TO_SECRET_TOKEN)
+        path_to_nitk_setting = os.getenv("PATH_TO_NITK_SETTING")
+        path_to_secret_token = os.getenv("PATH_TO_SECRET_TOKEN")
         res = run(["cp",path_to_nitk_setting,src],stdout=PIPE,stderr=PIPE)
         res = run(["cp",path_to_secret_token,src],stdout=PIPE,stderr=PIPE)
         src = f'{PATH_TO_HOME_DIR}/{org_name}/{repo_name}/{branch_name}/{repo_name}/'
