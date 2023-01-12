00236bbaaa5ee705f52428a91bd8c2649891f844
vinayakj02 <vinayakv02.j@gmail.com>
2023-01-12 16:46:11 +0530

    changed forms, added scripts gitignore, timezone

--- stagingserver/settings.py
+++ stagingserver/settings.py
@@ -121,12 +121,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
 
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
