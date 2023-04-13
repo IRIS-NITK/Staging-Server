7423196e4c041532f527ab4a266941b794da6c72
ut <utkarshrm568@gmail.com>
2023-04-13 19:50:31 +0530

    more cleanup

--- main/views.py
+++ main/views.py
@@ -1,14 +1,12 @@
+"""
+views for main
+"""
+import os
+import subprocess
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from allauth.socialaccount.models import SocialToken
-from github import Github
-from subprocess import PIPE, run
-import gitlab,json,time,os,subprocess
-from django.http import HttpResponse,StreamingHttpResponse
-from django.shortcuts import render,redirect
-from main.tasks.services import deploy_from_git, stop_container, deploy_from_git_template, clean_up, get_org_and_repo_name_v2
-from main.tasks import findfreeport
-from django.core import serializers
+from django.http import HttpResponse, StreamingHttpResponse
+from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from main.models import RunningInstance
 from django.template import Context, loader
@@ -27,11 +25,13 @@ response_header = loader.get_template("response_header.html")
 
 @login_required
 def container_logs(request, pk):
-    instance = RunningInstance.objects.get(pk = pk)
+    # pylint: disable=unused-argument
+    instance = RunningInstance.objects.get(pk=pk)
     container_name = f"{PREFIX}_{instance.organisation.lower()}_{instance.repo_name.lower()}_{instance.branch.lower()}"
     command = ["docker", "logs", "-f", container_name]
 
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def generate_stream():
         yield response_header.render({"purpose": "Starting"})
@@ -45,7 +45,6 @@ def container_logs(request, pk):
             logs = errors.decode("utf-8")
             yield logs
 
-
     response = StreamingHttpResponse(generate_stream())
     del response["Content-Length"]
     response["Cache-Control"] = "no-cache"
@@ -56,14 +55,15 @@ def container_logs(request, pk):
 @login_required
 def instance_logs(request, pk):
     try:
-        instance = RunningInstance.objects.get(pk = pk)
+        instance = RunningInstance.objects.get(pk=pk)
         log_file_name = f"{PATH_TO_HOME_DIR}/{instance.organisation}/{instance.repo_name}/{instance.branch}/{instance.branch}.txt"
-        with open(log_file_name, "r") as file:
+        with open(log_file_name, "r", encoding='UTF-8') as file:
             data = file.read()
-        context = {'data': data, 'instance' : instance }
+        context = {'data': data, 'instance': instance}
         return render(request, 'logs.html', context)
-    except:
+    except:  # pylint: disable=bare-except
         return render(request, 'error_log.html')
+
 
 @login_required
 def homepage(request):
