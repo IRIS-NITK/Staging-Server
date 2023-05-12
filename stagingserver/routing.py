f7c46f1db129f91daaa8cd72ef424ff18836b4ba
ut <utkarshrm568@gmail.com>
2023-05-13 04:01:16 +0530

    as per docs

--- stagingserver/routing.py
+++ stagingserver/routing.py
@@ -7,8 +7,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from main.urls import websocket_urlpatterns
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(websocket_urlpatterns)
