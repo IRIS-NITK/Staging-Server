fd7600995b265af49b2d6d2bacbdaf773bad0083
ut <utkarshrm568@gmail.com>
2023-05-13 20:54:45 +0530

    nginx reload remove

--- scripts/nginx_add_config_IRIS.sh
+++ scripts/nginx_add_config_IRIS.sh
@@ -14,11 +14,3 @@ PORT=$2
 
 sed -e "s/<BRANCH_NAME>/$BRANCH/g" -e "s/<PORT>/$PORT/g" sites-available/dev-template.conf > sites-available/dev-${BRANCH}.conf
 ln -f -s ../sites-available/dev-${BRANCH}.conf sites-enabled/dev-${BRANCH}.conf
-service nginx reload
-
-
-
-
-
-
-
