e9b1f37965a6ac16e4b2369baa684e909997ed71
ut <utkarshrm568@gmail.com>
2023-05-13 20:46:34 +0530

    some fixes

--- deploy/deploy-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000 > staging_logs.out
-gunicorn --bind 0.0.0.0:8000 --workers 3 stagingserver.wsgi:application
