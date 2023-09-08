caf3fa73664a1ddef9f9410b011ffcbe35fb0ae3
Devaansh-Kumar <devaanshk840@gmail.com>
2023-09-09 00:26:11 +0530

    edited deploy script to fix django migrations

--- deploy/gunicorn.sh
+++ deploy/gunicorn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py makemigrations
-python manage.py migrate
+# python manage.py migrate
+python manage.py migrate --run-syncdb
 gunicorn --bind 127.0.0.1:7000 --workers 3 stagingserver.wsgi:application
