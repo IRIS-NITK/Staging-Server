87f72364827b8b1a8b923834d938a0826e21a385
ut <utkarshrm568@gmail.com>
2023-10-08 16:29:01 +0530

    fixed

--- deploy/gunicorn.sh
+++ deploy/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations
-python manage.py migrate --run-syncdb
-gunicorn --bind 0.0.0.0:7000 --workers 3 stagingserver.wsgi:application --log-file=-
+python manage.py migrate
+gunicorn --bind 127.0.0.1:7000 --workers 3 stagingserver.wsgi:application
--- stagingserver/settings.py
+++ stagingserver/settings.py
@@ -17,7 +17,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -218,3 +217,23 @@ accept_content = ['application/json']
 result_serializer = 'json'
 task_serializer = 'json'
 timezone = 'Asia/Kolkata'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+
+    },
+    'loggers': {
+        'daphne': {
+            'handlers': [
+                'console',
+            ],
+            'level': 'INFO'
+        },
+    },
+}
