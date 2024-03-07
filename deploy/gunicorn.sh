05173fe9eb67ae20b5dda1b503045f756f5c49e4
Utkarsh Mahajan <utkarshrm568@gmail.com>
2024-03-07 16:38:52 +0530

    move services and network to ipv6

--- deploy/gunicorn.sh
+++ deploy/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
-gunicorn --bind 0.0.0.0:7000 --workers 3 stagingserver.wsgi:application --log-file=-
+gunicorn --bind '[::]:7000' --workers 3 stagingserver.wsgi:application --log-file=-
